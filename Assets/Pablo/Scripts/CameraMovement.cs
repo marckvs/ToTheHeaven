2af398704c56055dc074a94d3299ac5ff29b7304
pablogseg <pablogseg@users.noreply.github.com>
2017-11-11 20:58:12 +0100

    Phiro + Camera Scripts

---
+++ Assets/Pablo/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public Transform phiro;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = new Vector3(phiro.position.x, 0,-10);
+        transform.position = pos;
+        
+	}
+}

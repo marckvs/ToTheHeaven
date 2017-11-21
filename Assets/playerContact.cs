05a709e76d0fc6798c7dcbedec87f8e17d6ec723
oskarter9 <oscar5teruel@gmail.com>
2017-11-21 20:37:56 +0100

    Revert "Revert "Particulas y destruccion de estas con contador""
    This reverts commit 204f1ab7fa34186f5e30016f740c50daef79c40f.

---
+++ Assets/playerContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerContact : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Phiro")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}

6b341b9671f3954ed2ea3abf189251cd90925a99
oskarter9 <oscar5teruel@gmail.com>
2017-11-22 18:10:06 +0100

    Retoque iluminacion particulas

--- Assets/playerContact.cs
+++ Assets/playerContact.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class playerContact : MonoBehaviour {
 
     public Light lightBall;
+    public bool reducir;
     public float lightIntensityMax=10;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        reducir = false;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+		if (lightBall.intensity > lightIntensityMax)
+        {
+            reducir = true;
+        }
+
+        if (lightBall.intensity == 0)
+        {
+            reducir = false;
+        }
+
+        if (reducir)
+        {
+            lightBall.intensity -= 0.1f;
+        }
+
+        if (!reducir)
+        {
+            lightBall.intensity += 0.1f;
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {

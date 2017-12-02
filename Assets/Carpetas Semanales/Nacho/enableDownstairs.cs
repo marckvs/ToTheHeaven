b7e4d06a4f1afeecee72e48c0ac4920720b64b6a
Marc <al341516@uji.es>
2017-12-02 02:35:56 +0100

    Dialogos casi terminados

--- Assets/Carpetas Semanales/Nacho/enableDownstairs.cs
+++ Assets/Carpetas Semanales/Nacho/enableDownstairs.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class enableDownstairs : MonoBehaviour {
 
-    public PhiroMovement player;
+    private GameObject player;
+    private PhiroMovement phiroMov;
     public GameObject platform;
     private BoxCollider2D colliderPlatform;
 
 	// Use this for initialization
+    
 	void Start ()
     {
         colliderPlatform = platform.GetComponent<BoxCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Phiro");
+        phiroMov = player.GetComponent<PhiroMovement>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if (player.stairsWithPlatform)
+		if (phiroMov.stairsWithPlatform)
         {
             colliderPlatform.isTrigger = true;
         }
---
+++ Assets/TriggerDialogues.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogues : MonoBehaviour {
+
+    public GameObject GO;
+
+	void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Phiro")
+        {
+            GO.SetActive(true);
+        }
+    }
+}

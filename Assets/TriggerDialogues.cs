79f1e68e0293435d60f866499def2086366dd783
Marc <al341516@uji.es>
2017-12-03 13:52:40 +0100

    triggers aldea

--- Assets/TriggerDialogues.cs
+++ Assets/TriggerDialogues.cs
@@ -6,9 +6,16 @@ public class TriggerDialogues : MonoBehaviour {
 
     public GameObject GO;
 
-	void OnTriggerEnter2D(Collider2D c)
+    void OnTriggerEnter2D(Collider2D c)
     {
         if (c.gameObject.tag == "Phiro")
+        {
+            
+        }
+    }
+	void OnTriggerStay2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Phiro" && Input.GetKeyDown(KeyCode.F)) 
         {
             GO.SetActive(true);
         }
